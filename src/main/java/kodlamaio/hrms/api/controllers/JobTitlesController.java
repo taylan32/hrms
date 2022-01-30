40e79d1c05c250658b9078aacf8a80fbc86ac4bd
Taylan <taylanalparslan@hotmail.com>
2022-01-30 10:35:09 +0300

    ExceptionHandler eklendi

--- src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -6,13 +6,16 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -62,6 +65,8 @@ public class JobTitlesController {
 		return this.jobTitleService.delete(jobTitle);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public Map<String, String> handleValidationException(MethodArgumentNotValidException exceptions) {
 		Map<String, String> validationErrors = new HashMap<String, String>();
 		exceptions.getBindingResult().getAllErrors().forEach((error) -> {
